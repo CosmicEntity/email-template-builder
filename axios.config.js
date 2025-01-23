429f73408da998653d3651252bebc7f6a6411f3c
Vijay Mahto <cosmicentity19@gmail.com>
2025-01-23 12:26:32 +0530

    Axios baseURL changed

--- axios.config.js
+++ axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://email-template-builder-backend-4fer.onrender.com",
 });
 
 export default Axios;
